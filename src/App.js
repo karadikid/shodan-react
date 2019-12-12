af3df3028748520c768a238d67993143e6fb1a72
Gouthum Karadi <goukaradi@hotmail.com>
2019-12-12 16:07:22 -0500

    Successful API return static, no shodanArray.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Sample Query from Postman
+// https://api.shodan.io/shodan/host/search?key=6Ew4Z6p9kOwelROHLtOwn23ChCIoR0H1&query=port:1494,3389 org:"amazon web services"&facets=
+let apiKey = "8qzxm9PWXNeoWPhhIxtDm3xMt4Uzxr1m";
+let url = "https://api.shodan.io/shodan/host/search?key=";
+let value = "port:3389 80";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      shodanArray: [],
+      hereArray: []
+    };
+  }
+
+  buttonPress() {
+    fetch(`${url}${apiKey}&query=${value}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        // this.setState({shodanArray: res})
+        // console.log(this.state.shodanArray)
+      })
+  }
+
+  render() {
+    //Most important lesson: Array of Objects [Key: value, key:value, key: value] or [ :value, :value, :value],
+    //.map((arg1:item, arg2:index) => { key={index} item={item}})  ALWAYS
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="ShodanAPI">Welcome to KaradiShodan</h1>
+        </header>
+        <p className="App-intro"></p>
+        <Button click={this.buttonPress} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+
+render (){
+    return(
+        <div>
+        <button onClick={this.props.click}>
+            Search
+        </button>
+        </div>
+    );
+}
+
+}
+
+
+export default Button;
