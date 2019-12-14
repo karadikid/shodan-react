8d720924e5450df5eda869c8bcfc7a248ce6e611
Gouthum "Karate" Karadi <karadi@virtualnex.us>
2019-12-14 12:28:54 -0500

    Adding JSON Articles for Table.

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+//Columns in React-js
+//https://medium.com/@subalerts/create-dynamic-table-from-json-in-react-js-1a4a7b1146ef
+//https://stackoverflow.com/questions/38536399/creating-a-dynamic-table-from-json-response-in-reactjs
+//https://jsfiddle.net/combatmonk/7Ldrsg2h/1/
 
 class Table extends Component {
     constructor(props) {
